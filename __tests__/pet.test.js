543aa0fe2dfec8b143969d6acad1b7a789653080
Greg Cain <codegregcode@gmail.com>
2022-12-09 12:02:04 +0000

    writes tests and src for adding name to pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,4 +4,10 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual('Fido');
+        const pet2 = new Pet('Eddy');
+        expect(pet2.name).toEqual('Eddy');
+      });
   });
