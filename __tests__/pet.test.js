e0dc81b5621d16b695a1ff658012b927d09cc94d
Greg Cain <codegregcode@gmail.com>
2022-12-09 12:50:02 +0000

    writes test and src for growUp function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,12 +2,24 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
     it('sets the name property', () => {
         const pet = new Pet('Fido');
         expect(pet.name).toEqual('Fido');
         const pet2 = new Pet('Eddy');
         expect(pet2.name).toEqual('Eddy');
-      });
-  });
+    });
+    it('has an initial age of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    });
+});
+
+describe('growUp', () => {
+    it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    });
+});
